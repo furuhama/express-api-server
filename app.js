7b4672b206f58e573565c36297f19fe05c7d456f
furuhama <furuhama@users.noreply.github.com>
2019-02-17 17:23:09 +0900

    Define middleware for logging requests to console

--- app.js
+++ app.js
@@ -19,21 +19,27 @@ var languageList = [
   }
 ]
 
-// Define Routings
+// === Define middleware for a console logger ===
 
-// languages#index
-app.get("/api/languages", function(req, res, next){
+app.use(function(req, res, next) {
   console.log("protocol: " + req.protocol);
   console.log("method: " + req.method);
   console.log("originalUrl: " + req.originalUrl);
   console.log("hostname: " + req.hostname);
   console.log("body: " + req.body);
 
+  next()
+});
+
+// === Define Routings ===
+
+// languages#index
+app.get("/api/languages", function(req, res, next) {
   res.json(languageList);
 });
 
 // languages#show
-app.get("/api/languages/:languageId", function(req, res, next){
+app.get("/api/languages/:languageId", function(req, res, next) {
   let language;
 
   for (i = 0; i < languageList.length; i++){
