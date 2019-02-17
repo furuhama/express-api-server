b426a84f61dbf72877d98423942a771ec35805bb
furuhama <furuhama@users.noreply.github.com>
2019-02-17 18:09:17 +0900

    Init Sequelize for mysql & Set seed data on setup & Use it for a response

--- app.js
+++ app.js
@@ -1,23 +1,65 @@
-// Init Express application
+// === Initialize Express & Sequelize ===
+
 var express = require("express");
 var app = express();
 
-// Init server with port 3000
-var server = app.listen(3000, function(){
-  console.log("Express Server is listening to PORT: " + server.address().port);
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('sample_db', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+  operatorAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
-var languageList = [
-  {
-    id: "1",
-    name: "Ruby",
-    type: "Dynamic"
-  },{
-    id: "2",
-    name: "Rust",
-    type: "Static"
+// === Define models ===
+
+const Language = sequelize.define('languages', {
+  name: {
+    type: Sequelize.STRING
+  },
+  type: {
+    type: Sequelize.STRING
   }
-]
+});
+
+// Create seed data before server starts
+//
+// MEMO: This is not good practice.
+// However, I think it is better to be more simple and easier to understand this app.
+Language.sync({ force: true }).then(() => {
+  Language.create({
+    name: 'Ruby',
+    type: 'Dynamic'
+  });
+
+  Language.create({
+    name: 'Rust',
+    type: 'Static'
+  });
+});
+
+// == Start Server ===
+
+// Start server with port 3000
+var server = app.listen(3000, function(){
+  console.log("Express Server is listening to PORT: " + server.address().port);
+
+  // Check mysql database connection
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+      console.error('Unable to connect to the database: ', err);
+    });
+});
 
 // === Define middleware for a console logger ===
 
@@ -35,19 +77,22 @@ app.use(function(req, res, next) {
 
 // languages#index
 app.get("/api/languages", function(req, res, next) {
-  res.json(languageList);
+  Language.findAll()
+    .then(languages => {
+      res.json(languages);
+    })
+    .catch(err => {
+      console.error('Failed to get data from database: ', err);
+    });
 });
 
 // languages#show
 app.get("/api/languages/:languageId", function(req, res, next) {
-  let language;
-
-  for (i = 0; i < languageList.length; i++){
-    // You can use parameter with `req.params.PARAMETER_NAME`
-    if (languageList[i].id == req.params.languageId){
-      language = languageList[i];
-    }
-  }
-
-  res.json(language);
+  Language.findByPk(req.params.languageId)
+    .then(language => {
+      res.json(language);
+    })
+    .catch(err => {
+      console.error('Failed to get data from database: ', err);
+    });
 });
